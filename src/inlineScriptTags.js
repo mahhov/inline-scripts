6ded1d25fea7cd675b4c0956bb8a3b925e0fb20d
George Davies <george.davies@capgemini.com>
2020-07-16 18:55:19 +0100

    Changed inlineScriptTags regex to accept script tags with additional attributes and added short circuit if tags are found

--- src/inlineScriptTags.js
+++ src/inlineScriptTags.js
@@ -4,10 +4,13 @@ const fs = require('fs').promises;
 const path = require('path');
 
 let inlineHtmlScripts = async htmlPath => {
-	const scriptTagRegex = /<script src="([\w.\-\/]+)"><\/script>/;
+	const scriptTagRegex = /<script .*src="([\w.\-\/]+)".*><\/script>/;
 	let html = await fs.readFile(htmlPath, 'utf8');
-	let scriptPromises = html
-		.match(new RegExp(scriptTagRegex, 'g'))
+
+        let matches = html.match(new RegExp(scriptTagRegex, 'g'));
+        if (matches == null) return html;
+
+	let scriptPromises = matches
 		.map(scriptTag => scriptTag.match(scriptTagRegex)[1])
 		.map(relScriptPath => path.resolve(path.dirname(htmlPath), relScriptPath))
 		.map(scriptPath => fs.readFile(scriptPath, 'utf8'));
