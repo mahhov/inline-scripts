cd1f55fa94cd14bc2952f286ef16504ab66a2e35
Roland Fredenhagen <dev@modprog.de>
2022-02-19 12:41:08 +0100

    export all functionality for js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 let inlineScriptTags = require('./inlineScriptTags');
 let inlineRequires = require('./inlineRequires');
 let inlineEnvironmentVariables = require('./inlineEnvironmentVariables');
+let inlineImages = require('./inlineImages');
+let inlineStylesheets = require('./inlineStylesheets');
 
-module.exports = {inlineScriptTags, inlineRequires, inlineEnvironmentVariables};
+module.exports = {inlineScriptTags, inlineRequires, inlineEnvironmentVariables, inlineImages, inlineStylesheets};
